bbed6e819df11fb4edac2d6f53abae71c509f6a9
haider <haiderfuad33@gmail.com>
2025-02-14 17:50:15 +0300

    fix(security): remove Supabase keys and url from repo to .env file

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://aidighmswvkcdvgoxeze.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpZGlnaG1zd3ZrY2R2Z294ZXplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkwMzY1OTIsImV4cCI6MjA1NDYxMjU5Mn0.3ulXeKuuItshDTz_Nt1ZkN_khNMDKqMdFxuvOlwR4Sg";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
